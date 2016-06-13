1c3a4eb97e05d78eab0a591fdfc06176bb0000f9
Mairin Bailey <mairin.bailey@gmail.com>
2016-06-13 15:14:26 -0600

    :sparkles:

---
+++ seeds/track.js
@@ -0,0 +1,8 @@
+exports.seed = function(knex, Promise){
+  return Promise.join(
+    knex('track').del(),
+
+    knex('track').insert({id: 1, name: 'Strawberry Fields'}),
+    knex('track').insert({id: 2, name:'Soft Bulletin'})
+  );
+};
