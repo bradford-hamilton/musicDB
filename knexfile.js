63f6dc7c75c87251efd4a957a2783bce4124fffe
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2016-06-13 16:32:26 -0600

    newest changes

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,7 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/music'
+    // connection: 'postgres://whbqicagocdkoq:Wd9Fd8xgBMrxuGH5cy_YI31UYH@ec2-54-235-68-4.compute-1.amazonaws.com:5432/d6vd8094rssa4v?ssl=true'
   },
 
   production: {
--- seeds/track.js
+++ seeds/track.js
@@ -2,7 +2,7 @@ exports.seed = function(knex, Promise){
   return Promise.join(
     knex('track').del(),
 
-    knex('track').insert({id: 1, name: 'Strawberry Fields'}),
-    knex('track').insert({id: 2, name:'Soft Bulletin'})
+    knex('track').insert({id: 1, name: 'Strawberry Fields', artist_id: 1}),
+    knex('track').insert({id: 2, name:'Soft Bulletin', artist_id: 2})
   );
 };
